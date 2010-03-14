d80c0527df674084d787517ba6808789b91dcbb4
Daniel Schauenberg <d@unwiredcouch.com>
2010-03-14 03:20:43 +0100

    add basic class for template

---
+++ include/template.hpp
@@ -0,0 +1,19 @@
+/**
+ * @file template.hpp
+ * @brief header file for plustache template
+ * @author Daniel Schauenberg <d@unwiredcouch.com>
+ * @version 0.1
+ * @date 2010-03-12
+ */
+
+#include <stdio.h>
+
+class template_t {
+public:
+    template_t ();
+    virtual ~template_t ();
+    void hello_world();
+
+private:
+    /* data */
+};
---
+++ src/template.cpp
@@ -0,0 +1,27 @@
+/**
+ * @file template.cpp
+ * @brief plustache template
+ * @author Daniel Schauenberg <d@unwiredcouch.com>
+ * @version 0.1
+ * @date 2010-03-12
+ */
+
+#include <template.hpp>
+#include <iostream>
+
+using namespace std;
+
+void template_t::hello_world()
+{
+    cout << "Hello World!\n";
+}
+
+template_t::template_t()
+{
+
+}
+
+template_t::~template_t()
+{
+
+}

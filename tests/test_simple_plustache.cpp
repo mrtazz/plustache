2b1d937bb9a9d9eae36b2fdac7d6dfd23ec19e1c
Daniel Schauenberg <d@unwiredcouch.com>
2010-03-26 01:37:10 +0100

    implement test for simple plustache render

--- tests/test_simple_plustache.cpp
+++ tests/test_simple_plustache.cpp
@@ -1,35 +1,40 @@
-#include "template.h"
+#include "template.hpp"
 #include <gtest/gtest.h>
 
-namespace {
-
 // The fixture for testing class Foo.
-class SimpleTest : public ::testing::Test {
+class SimpleTest : public ::testing::Test
+{
  protected:
     string result;
+    string template_string;
+    context ctx;
 
-    SimpleTest() {
+    SimpleTest()
+    {
     }
 
-    virtual ~SimpleTest() {
+    virtual ~SimpleTest()
+    {
     }
 
-    virtual void SetUp() {
+    virtual void SetUp()
+    {
+        template_string = "text {{title}} text";
+        ctx["title"] = "replaced";
         template_t t;
-        result = t.render(template_string,context);
+        result = t.render(template_string, ctx);
     }
 
-    virtual void TearDown() {
+    virtual void TearDown()
+    {
     }
 
 };
 
 // Tests that a simple mustache tag is replaced
-TEST_F(SimpleTest, TestSimpleMustache) {
-    const string expected = "text replaced text"
-    ASSERT_EQ(result, expected);
+TEST_F(SimpleTest, TestSimpleMustache)
+{
+    const string expected = "text replaced text";
+    EXPECT_EQ(result, expected);
 }
-
-
-}  // namespace
 
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,7 +1,7 @@
-#include <iostream.h>
+#include <iostream>
 #include <gtest/gtest.h>
 
-int _tmain(int argc, _TCHAR* argv[])
+int main(int argc, char **argv)
 {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();

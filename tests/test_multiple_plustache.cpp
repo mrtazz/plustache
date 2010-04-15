649f21544e6fe358f4abded31d0f8e216bb2bcd3
Daniel Schauenberg <d@unwiredcouch.com>
2010-04-15 23:16:37 +0200

    add unit test for multiple mustaches

---
+++ tests/test_multiple_plustache.cpp
@@ -0,0 +1,43 @@
+#include "template.hpp"
+#include <gtest/gtest.h>
+
+// The fixture for testing class Foo.
+class MultipleTest : public ::testing::Test
+{
+ protected:
+    string result;
+    string template_string;
+    map<string, string> ctx;
+
+    MultipleTest()
+    {
+    }
+
+    virtual ~MultipleTest()
+    {
+    }
+
+    virtual void SetUp()
+    {
+        template_string = "Hi I am {{name}}.\n";
+        template_string += "I like {{pet}}.";
+        ctx["name"] = "Daniel";
+        ctx["pet"] = "turtles";
+        template_t t;
+        result = t.render(template_string, ctx);
+    }
+
+    virtual void TearDown()
+    {
+    }
+
+};
+
+// Tests that a simple mustache tag is replaced
+TEST_F(MultipleTest, TestMultipleMustache)
+{
+    string expected = "Hi I am Daniel.\n";
+          expected += "I like turtles.";
+    EXPECT_EQ(result, expected);
+}
+

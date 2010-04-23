64940bf9d98ce4a90767a7a44656eac3050d66fb
Daniel Schauenberg <d@unwiredcouch.com>
2010-04-23 09:15:55 +0200

    improve/fix html escape unit test

--- tests/test_html_escape_plustache.cpp
+++ tests/test_html_escape_plustache.cpp
@@ -7,11 +7,15 @@
 class HtmlEscapeTest : public ::testing::Test
 {
  protected:
-    string result_string;
-    string result_file;
-    string template_string;
+    string result_escaped_string;
+    string result_escaped_file;
+    string result_unescaped_string;
+    string result_unescaped_file;
+    string escaped_string;
+    string unescaped_string;
     map<string, string> ctx;
-    string file;
+    string escaped_file;
+    string unescaped_file;
 
     HtmlEscapeTest()
     {
@@ -23,25 +27,37 @@ class HtmlEscapeTest : public ::testing::Test
 
     virtual void SetUp()
     {
-        template_string = "text {{{title}}} text\n";
-        template_string += "text {{& title}} text\n";
-        template_string += "text {{ title}} text";
-        file = "multiple.mustache";
+        escaped_string = "text {{ title }} text\n";
+        escaped_string += "text {{ ritle }} text";
+        unescaped_string = "text {{{ title }}} text\n";
+        unescaped_string += "text {{& ritle }} text";
+        unescaped_file = "unescaped.mustache";
+        escaped_file = "escaped.mustache";
 
-        std::ofstream myfile;
-        myfile.open (file.c_str());
-        myfile << template_string;
-        myfile.close();
+        // escaped
+        std::ofstream myfile_escaped;
+        myfile_escaped.open(escaped_file.c_str());
+        myfile_escaped << escaped_string;
+        myfile_escaped.close();
+        // unescaped
+        std::ofstream myfile_unescaped;
+        myfile_unescaped.open(unescaped_file.c_str());
+        myfile_unescaped << unescaped_string;
+        myfile_unescaped.close();
 
         ctx["title"] = "<pre>\"\\&</pre>";
+        ctx["ritle"] = "<pre>\"\\&</pre>";
         template_t t;
-        result_string = t.render(template_string, ctx);
-        result_file = t.render(file, ctx);
+        result_escaped_string = t.render(escaped_string, ctx);
+        result_escaped_file = t.render(escaped_file, ctx);
+        result_unescaped_string = t.render(unescaped_string, ctx);
+        result_unescaped_file = t.render(unescaped_file, ctx);
     }
 
     virtual void TearDown()
     {
-        remove(file.c_str());
+        remove(escaped_file.c_str());
+        remove(unescaped_file.c_str());
     }
 
 };
@@ -50,15 +66,27 @@ class HtmlEscapeTest : public ::testing::Test
 TEST_F(HtmlEscapeTest, TestHtmlEscapeMustacheFromString)
 {
     string expected = "text &lt;pre&gt;&quot;&#92;&amp;&lt;/pre&gt; text\n";
-    expected += "text &lt;pre&gt;&quot;&#92;&amp;&lt;/pre&gt; text\n";
-    expected += "text <pre>\"\\&</pre> text\n";
-    EXPECT_EQ(expected, result_string);
+    expected += "text &lt;pre&gt;&quot;&#92;&amp;&lt;/pre&gt; text";
+    EXPECT_EQ(expected, result_escaped_string);
 }
 
 TEST_F(HtmlEscapeTest, TestHtmlEscapeMustacheFromFile)
 {
     string expected = "text &lt;pre&gt;&quot;&#92;&amp;&lt;/pre&gt; text\n";
-    expected += "text &lt;pre&gt;&quot;&#92;&amp;&lt;/pre&gt; text\n";
-    expected += "text <pre>\"\\&</pre> text\n";
-    EXPECT_EQ(expected, result_file);
+    expected += "text &lt;pre&gt;&quot;&#92;&amp;&lt;/pre&gt; text";
+    EXPECT_EQ(expected, result_escaped_file);
+}
+
+TEST_F(HtmlEscapeTest, TestHtmlUnEscapeMustacheFromString)
+{
+    string expected = "text <pre>\"\\&</pre> text\n";
+    expected += "text <pre>\"\\&</pre> text";
+    EXPECT_EQ(expected, result_unescaped_string);
+}
+
+TEST_F(HtmlEscapeTest, TestHtmlUnEscapeMustacheFromFile)
+{
+    string expected = "text <pre>\"\\&</pre> text\n";
+    expected += "text <pre>\"\\&</pre> text";
+    EXPECT_EQ(expected, result_unescaped_file);
 }

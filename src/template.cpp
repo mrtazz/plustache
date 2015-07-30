5f13c29f140462f8dd829498627005df9d447620
Kyle Alan Hale <khale@ancestry.com>
2015-07-30 08:36:35 -0700

    Add comments to new methods

--- src/template.cpp
+++ src/template.cpp
@@ -16,24 +16,42 @@
 using namespace Plustache;
 
 
-// trim from start
-static inline std::string &ltrim(std::string &s) 
+/**
+ * @brief trim whitespace from the start of a string
+ *
+ * @param s the string to trim
+ *
+ * @return s, trimmed
+ */
+static inline std::string &ltrim(std::string &s)
 {
     s.erase(s.begin(), std::find_if(s.begin(), s.end(),
         std::not1(std::ptr_fun<int, int>(std::isspace))));
     return s;
 }
 
-// trim from end
-static inline std::string &rtrim(std::string &s) 
+/**
+ * @brief trim whitespace from the end of a string
+ *
+ * @param s the string to trim
+ *
+ * @return s, trimmed
+ */
+static inline std::string &rtrim(std::string &s)
 {
     s.erase(std::find_if(s.rbegin(), s.rend(),
         std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
     return s;
 }
 
-// trim from both ends
-static inline std::string &trim(std::string &s) 
+/**
+ * @brief trim whitespace from both ends of a string
+ *
+ * @param s the string to trim
+ *
+ * @return s, trimmed
+ */
+static inline std::string &trim(std::string &s)
 {
     return ltrim(rtrim(s));
 }
@@ -79,6 +97,10 @@ void template_t::compile_data()
     ctag = "\\}\\}";
     update_tags();
 }
+/**
+ * @brief applies the current ctag/otag settings to the tag & section
+ *   regex patterns
+ */
 void template_t::update_tags() {
     // tag and section regex
     tag.assign(otag + "(#|=|&|!|>|\\{)?(.+?)(\\})?" + ctag);

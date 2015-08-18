6922191cf9a04c05cac053dff15b2d42e6dbe6f1
John Chesley <john@chesl.es>
2015-08-18 10:43:46 -0400

    fix bug with special characters in replacement text
    this closes #20, closes #17
    
    use boost format_literal flag to suppress regex_replace handling special characters
    
    add failing test case for dollar sign bug
    
    add failing test case for disappearing parens (#17)

--- tests/test_simple_plustache.cpp
+++ tests/test_simple_plustache.cpp
@@ -15,6 +15,10 @@ class SimpleTest : public ::testing::Test
     std::string template_string;
     std::string result_notfound;
     std::string notfound;
+    std::string result_dollars;
+    std::string dollars;
+    std::string result_parens;
+    std::string parens;
     std::map<std::string, std::string> ctx;
     std::string file;
 
@@ -30,6 +34,8 @@ class SimpleTest : public ::testing::Test
     {
         template_string = "text {{title}} text";
         notfound = "text {{fitle}} text";
+        dollars = "text {{dollars}}";
+        parens = "text {{ open_paren }}inside{{ close_paren }}";
         file = "multiple.mustache";
 
         std::ofstream myfile;
@@ -38,10 +44,16 @@ class SimpleTest : public ::testing::Test
         myfile.close();
 
         ctx["title"] = "replaced";
+        ctx["dollars"] = "$0";
+        ctx["open_paren"] = "(";
+        ctx["close_paren"] = ")";
+
         Plustache::template_t t;
         result_string = t.render(template_string, ctx);
         result_file = t.render(file, ctx);
         result_notfound = t.render(notfound, ctx);
+        result_dollars = t.render(dollars, ctx);
+        result_parens = t.render(parens, ctx);
     }
 
     virtual void TearDown()
@@ -67,4 +79,16 @@ TEST_F(SimpleTest, TestSimpleNotFoundMustacheFromString)
 {
     const std::string expected = "text  text";
     EXPECT_EQ(expected, result_notfound);
+}
+
+TEST_F(SimpleTest, TestDollarSignsInValue)
+{
+    const std::string expected = "text $0";
+    EXPECT_EQ(expected, result_dollars);
+}
+
+TEST_F(SimpleTest, TestParensInValue)
+{
+    const std::string expected = "text (inside)";
+    EXPECT_EQ(expected, result_parens);
 }
